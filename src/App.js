8b28ba0f323b0396bc76b4f6649a19ee79db9337
thienpa@fabbi.io <no.dev.company@gmail.com>
2025-03-31 17:33:49 +0700

    fix error

--- src/App.js
+++ src/App.js
@@ -2,11 +2,9 @@ import React from "react";
 import Header from "./components/Header";
 import CardCollection from "./components/CardCollection";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Analytics } from "@vercel/analytics/remix";
 function App() {
   return (
     <div className="App">
-      <Analytics />
       <Header />
       <CardCollection />
     </div>
